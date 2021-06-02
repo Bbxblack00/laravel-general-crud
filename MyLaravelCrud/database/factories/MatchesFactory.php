f3c16dcb36bf3cabcc7f5f1701e4d6868c1fd727
Benito <bbxblack@gmail.com>
2021-06-02 23:54:40 +0200

    v0.3

--- MyLaravelCrud/database/factories/MatchesFactory.php
+++ MyLaravelCrud/database/factories/MatchesFactory.php
@@ -7,10 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        'team1' => $faker -> , 
-        'team2' => $faker -> ,
-        'point1' => $faker -> ,
-        'point2' => $faker -> ,
-        'result' => $faker -> ,
+        'team1' => $faker -> word, 
+        'team2' => $faker -> word,
+        'point1' => $faker -> numberBetween($min = 0, $max = 20),
+        'point2' => $faker -> numberBetween($min = 0, $max = 20),
+        'result' => $faker -> numberBetween($min = 0, $max = 1)? 'ha vinto la prima squadra' : 'ha vinto la seconda squadra',
     ];
 });
