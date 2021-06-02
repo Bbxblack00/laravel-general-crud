85a4c502aa6c7056e5c1ee3cc2a6608c894e8aa4
Benito <bbxblack@gmail.com>
2021-06-02 23:34:41 +0200

    v0.1

--- MyLaravelCrud/app/Http/Controllers/MatchController.php
+++ MyLaravelCrud/app/Http/Controllers/MatchController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Match;
 
 class MatchController extends Controller
 {
-    //
+    public function home(){
+
+        return view('pages.home', compact());
+    }
 }
